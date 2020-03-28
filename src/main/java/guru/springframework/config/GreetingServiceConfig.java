d184845cd8adfdf89cdcb1842bfaec2212eb12e6
pratapchowdari kolla <kollapratapchowdari@gmail.com>
2020-03-28 13:39:41 -0400

    java config changes

---
+++ src/main/java/guru/springframework/config/GreetingServiceConfig.java
@@ -0,0 +1,42 @@
+package guru.springframework.config;
+
+import guru.springframework.controllers.GetterInjectedController;
+import guru.springframework.services.GreetingRepository;
+import guru.springframework.services.GreetingService;
+import guru.springframework.services.GreetingServiceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    GreetingServiceFactory greetingRepository(GreetingRepository greetingRepository){
+        return new GreetingServiceFactory(greetingRepository);
+    }
+
+    @Bean
+    @Primary
+    @Profile({"default","en"})
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    @Bean
+    @Primary
+    @Profile("de")
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("de");
+    }
+
+    @Bean
+    @Primary
+    @Profile("es")
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("es");
+    }
+
+}
